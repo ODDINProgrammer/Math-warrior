048659f2f661de230f31fb723adc8ca14c95ef94
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2021-11-25 01:19:13 +0200

    Adding UI
    Added UI panel to display enemy.
    Added test attack button.
    Added TextMeshPro.
    Added more visuals and UI icons.

---
+++ Math warrior/Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] internal int _maxHP;
+    private int _currentHP;
+
+    [SerializeField] internal int _damage;
+
+
+    [TextArea][SerializeField] internal string _description;
+}
--- Math warrior/Assets/Scripts/Player.cs
+++ Math warrior/Assets/Scripts/Player.cs
@@ -6,11 +6,16 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private int _maxHP;
     private int _currentHP;
+
+    private Enemy _currentEnemy;
+
+    [SerializeField] private GameManager _gameManager;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.CompareTag("Enemy"))
         {
             GetComponent<Player_move>().StopPlayer();
+            _currentEnemy = collision.transform.GetComponent<Enemy>();
         }
     }
 }
